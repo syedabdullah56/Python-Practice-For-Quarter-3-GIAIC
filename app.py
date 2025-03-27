9f4d12b56fc3543c3f714b725ccb89ec24f184cb
syedabdullah56 <syedabdullah8750@gmail.com>
2025-03-27 06:51:35 +0500

    py_00 completed from panaverse repo

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # 00_google_colab
+
+# Google Colab Tutorial: A Detailed Guide for Beginners
+# Google Colab, or Colaboratory, is a free, cloud-based environment that allows you to write and execute Python code in your browser. It’s particularly popular for machine learning and data science projects because it provides free access to GPUs and TPUs, making it a powerful tool for prototyping and experimenting with code. This tutorial will walk you through the basics of using Google Colab, from setting up your environment to executing code and sharing your work.
+
+# This repo is all about Google Colab interface and to be familiar with Google Colab environment.
+
+# Markdown Basics
+
+#    #--> for headers
+#    wrap in ** --> for bold
+#    wrap in *--> for italic
+#    use - with space or 1. with space to form a list
+
+#Completed
