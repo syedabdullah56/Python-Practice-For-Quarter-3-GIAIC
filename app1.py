75fcf5b2edefbd4a9f75055c0801936f4614f60f
syedabdullah56 <syedabdullah8750@gmail.com>
2025-03-28 07:11:27 +0500

    py-01 Intro To Python Done from Panaverse Repo also created a visualization of how python code works

--- app1.py
+++ app1.py
@@ -1,1 +1,24 @@
 # 01_Intro_to_Python
+#Python is an interpreted language it is executed line by line in machine code.
+
+#How it works:
+
+# Step-by-Step Execution of Python Code
+# Writing the Python Code
+# You write a .py file containing Python code.
+
+# Lexical Analysis (Tokenizer)
+# The Python interpreter breaks the code into tokens (keywords, variables, operators, etc.).
+
+# Parsing (Syntax Analysis)
+# The tokens are checked for syntax correctness and converted into an Abstract Syntax Tree (AST).
+
+# Compilation to Bytecode
+# The AST is compiled into bytecode (.pyc file in __pycache__), which is an intermediate representation.
+
+# Execution by Python Virtual Machine (PVM)
+# The Python Virtual Machine (PVM) reads the bytecode and executes it by interacting with the system's hardware (CPU, Memory).
+
+# Machine Code Execution
+# Finally, the system’s hardware runs the instructions, producing the desired output.
+#   .py file ===>  Tokenizer(Lexical)  ===> Parser(AST)  ===> Bytecode Compilation ===> Machine Code  ===> Final Output
